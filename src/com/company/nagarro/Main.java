39b4833f2c3fbd694682a883cebd0fc7a8873114
vikas0004 <vikas4.vb@gmail.com>
2021-08-25 16:22:48 +0530

    final commit

--- src/com/company/nagarro/Main.java
+++ src/com/company/nagarro/Main.java
@@ -5,6 +5,21 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("hello world");
-        //my name is vikas bhalla fuck poota.. 
+        //my name is vikas bhalla fuck poota..
+        String str = "malayalam";
+        System.out.println(checkPalindrome(str));
+    }
+
+    public static boolean checkPalindrome(String str)
+    {
+        int n = str.length();
+        for(int i = 0; i < n; i++)
+        {
+            if(str.charAt(i) != str.charAt(n-i-1))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
